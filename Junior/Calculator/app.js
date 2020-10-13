f8f52502993c4927c6ca367bdbcd75a5f206ff95
IsidoroAros <arostegui.isidoro@gmail.com>
2020-10-13 01:36:55 -0300

    .

---
+++ Junior/Calculator/app.js
@@ -0,0 +1,7 @@
+function display(){
+    const screen = document.getElementById('calculator-screen');
+    const number = document.getElementsByClassName('number').addEventListener('click',(e) {
+        console.log('hola')
+    });
+
+}
