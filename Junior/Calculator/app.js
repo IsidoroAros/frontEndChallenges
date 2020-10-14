06591d6f8f7afc69cbbbdd0c515a2bcde3deb713
Isidoro Aróstegui <arostegui.isidoro@gmail.com>
2020-10-14 13:28:43 -0300

    Finished calculator

--- Junior/Calculator/app.js
+++
@@ -1,7 +0,0 @@
-function display(){
-    const screen = document.getElementById('calculator-screen');
-    const number = document.getElementsByClassName('number').addEventListener('click',(e) {
-        console.log('hola')
-    });
-
-}
